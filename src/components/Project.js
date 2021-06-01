bd628d84904941060562d8dac28c6fc3ef48220c
Sabine Holler <sabidasa@gmail.com>
2021-06-01 16:48:28 +0200

    single post

--- src/components/Project.js
+++ src/components/Project.js
@@ -24,8 +24,8 @@ const Project = () => {
     return (
         <main className='bg-yellow-50 min-h-screen p-12'>
             <section className='container mx-auto'>
-                <h1 className='text-5xl flex justify-center cursive'> Projects </h1>
-                <h2 className='text-lg text-gray-600 flex justify-center mb-12'>is</h2>
+                <h1 className='text-5xl flex justify-center cursive mb-12'> Projects </h1>
+                {/* <h2 className='text-lg text-gray-600 flex justify-center mb-12'> Check out my projects!</h2> */}
                 <section className='grid grid-cols-2 gap-8'> 
                 {projectData && projectData.map((project, index) =>  (
                     <article className='relative rounded-lg shadow-xl bg-white p-16'> 
